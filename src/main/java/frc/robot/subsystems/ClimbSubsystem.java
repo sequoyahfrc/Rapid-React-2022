7d69f3cdd7c5e289071bd09a59a06d4d6a1bcbdb
Sequoyah PCs <sequoyahpcs@sequoyahrobotics.com>
2022-03-07 15:46:37 -0500

    solenoid works

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -2,26 +2,31 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubsystem extends SubsystemBase {
 
 	private final WPI_TalonFX motor;
-	private final DoubleSolenoid solenoid;
+	private final Solenoid forward, reverse;
 
 	public ClimbSubsystem(int motorID, int solenoidID) {
 		motor = new WPI_TalonFX(motorID);
-		solenoid = new DoubleSolenoid(0, PneumaticsModuleType.CTREPCM, 4, 5);
+		forward = new Solenoid(0, PneumaticsModuleType.CTREPCM, 4);
+		reverse = new Solenoid(0, PneumaticsModuleType.CTREPCM, 5);
+		motor.setInverted(true);
 	}
 
 	public void setMotor(double speed) {
 		motor.set(speed);
 	}
 
-	public void setSolenoid(boolean v) {
-		solenoid.set(v ? Value.kForward : Value.kReverse);
+	public void setForwardSolenoid(boolean v) {
+		forward.set(v);
+	}
+
+	public void setReverseSolenoid(boolean v) {
+		reverse.set(v);
 	}
 }
