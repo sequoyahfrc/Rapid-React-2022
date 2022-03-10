9fe0f8f7130f1c603633a88d6ce2b4c5b2f375b9
Sequoyah PCs <sequoyahpcs@sequoyahrobotics.com>
2022-03-10 18:26:45 -0500

    speed graph

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -19,6 +19,21 @@ public class DriveCommand extends CommandBase {
 	public void execute() {
 		double l = controller.getRawAxis(XboxController.Axis.kLeftY.value);
 		double r = controller.getRawAxis(XboxController.Axis.kRightY.value);
-		driveSubsystem.tankDrive(l * l * (l < 0 ? -1 : 1), r * r * (r < 0 ? -1 : 1));
+		driveSubsystem.tankDrive(speedGraph(l), speedGraph(r));
+	}
+
+	private static double speedGraph(double x) {
+		// Before changing, make sure graph(1) = 1 and graph(-1) = -1
+		final double a = 0.75;
+		final double b = 0.3333333333333333333;
+		final double c = 3.0;
+		boolean neg = x < 0;
+		x = Math.abs(x);
+		double y;
+		if (x < 0.75) {
+			y = a * x;
+		}
+		y = a * b + c * (x - a);
+		return y * (neg ? -1 : 1);
 	}
 }
